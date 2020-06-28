f86bcdad1819ea23d9589d363c29283b448f5106
Kwarn <karlwarner.dev@gmail.com>
2020-06-28 15:29:33 +0100

    Working on error handling

--- src/Hoc/withErrorHandler/withErrorHandler.js
+++ src/Hoc/withErrorHandler/withErrorHandler.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+
 import Modal from '../../components/UI/Modal/Modal'
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  return props => {
-    return (
-      <>
-        <Modal show>Something didn't work</Modal>
-        <WrappedComponent {...props} />
-      </>
-    )
+  return class extends Component {
+    state = {
+      error: null,
+    }
+
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null })
+        return req
+      })
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error })
+        }
+      )
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null })
+    }
+
+    render() {
+      return (
+        <>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </>
+      )
+    }
   }
 }
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -138,4 +138,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default withErrorHandler(BurgerBuilder)
+export default withErrorHandler(BurgerBuilder, axios)
