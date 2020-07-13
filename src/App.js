51522b3c5c13a41024be66a715c2aafbba0d40dc
Kwarn <karlwarner.dev@gmail.com>
2020-07-13 14:43:19 +0100

    Logout Implimented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
+import Logout from './containers/Auth/Logout/Logout'
 import './App.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/login" component={Auth} />
+          <Route path="/logout" component={Logout} />
           <Route path="/" component={BurgerBuilder} />
         </Switch>
       </Layout>
