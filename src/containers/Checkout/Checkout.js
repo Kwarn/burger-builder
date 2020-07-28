bfa9dfef35edbc846bf9c5f789564b9e6d646aae
Kwarn <karlwarner.dev@gmail.com>
2020-07-28 16:06:15 +0100

    converted Orders & Checkout to functional components

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
 
-class Checkout extends Component {
-  checkoutCancelledHandler = () => this.props.history.goBack()
+const Checkout = props => {
+  const checkoutCancelledHandler = () => props.history.goBack()
 
-  checkoutContinuedHandler = () =>
-    this.props.history.replace('/checkout/contact-data')
+  const checkoutContinuedHandler = () =>
+    props.history.replace('/checkout/contact-data')
 
   // redirects user if /contact-data is accessed via URL and burger ingredients is empty
   // redirects user to '/orders' when order is successfully posted to db
-  render() {
-    let summary = <Redirect to="/" />
-    if (this.props.ingredients) {
-      const shouldRedirect = this.props.shouldRedirect ? (
-        <Redirect to="/orders" />
-      ) : null
 
-      summary = (
-        <>
-          {shouldRedirect}
-          <CheckoutSummary
-            checkoutCancelled={this.checkoutCancelledHandler}
-            checkoutContinued={this.checkoutContinuedHandler}
-            ingredients={this.props.ingredients}
-          />
-          <Route
-            path={this.props.match.path + '/contact-data'}
-            component={ContactData}
-          />
-        </>
-      )
-    }
-    return summary
+  let summary = <Redirect to="/" />
+  if (props.ingredients) {
+    const shouldRedirect = props.shouldRedirect ? (
+      <Redirect to="/orders" />
+    ) : null
+
+    summary = (
+      <>
+        {shouldRedirect}
+        <CheckoutSummary
+          checkoutCancelled={checkoutCancelledHandler}
+          checkoutContinued={checkoutContinuedHandler}
+          ingredients={props.ingredients}
+        />
+        <Route
+          path={props.match.path + '/contact-data'}
+          component={ContactData}
+        />
+      </>
+    )
   }
+  return summary
 }
 
 const mapStateToProps = state => {
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import axios from '../../axios-orders'
 import { connect } from 'react-redux'
 import * as actions from '../../store/actions/index'
@@ -6,40 +6,38 @@ import withErrorHandler from '../../Hoc/withErrorHandler/withErrorHandler'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import Order from '../../components/Order/Order'
 
-class Orders extends Component {
-  // fetches orders from db
-  componentDidMount() {
-    this.props.onFetchOrders(this.props.token, this.props.userId)
-  }
+const Orders = props => {
+  const { onFetchOrders, token, userId } = props
+  useEffect(() => {
+    onFetchOrders(token, userId)
+  }, [onFetchOrders, token, userId])
 
-  // conditionally renders Spinner/orders if fetching ingreedients is successful
-  render() {
-    let orders = <Spinner />
-    if (this.props.orders) {
-      orders = this.props.orders.map(order => (
-        <Order
-          key={order.id}
-          ingredients={order.ingredients}
-          totalPrice={order.totalPrice}
-          orderDate={order.orderDate}
-        />
-      ))
-    }
-    return <div>{orders}</div>
+  let orders = <Spinner />
+  if (props.orders) {
+    orders = props.orders.map(order => (
+      <Order
+        key={order.id}
+        ingredients={order.ingredients}
+        totalPrice={order.totalPrice}
+        orderDate={order.orderDate}
+      />
+    ))
   }
+  return <div>{orders}</div>
 }
 
 const mapStateToProps = state => {
   return {
     orders: state.orders.orders,
     token: state.auth.token,
-    userId: state.auth.userId
+    userId: state.auth.userId,
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId)),
+    onFetchOrders: (token, userId) =>
+      dispatch(actions.fetchOrders(token, userId)),
   }
 }
 
