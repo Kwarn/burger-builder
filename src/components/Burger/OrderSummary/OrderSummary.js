bc1dde3337c7861f341739f33658918db5cfa5cb
Kwarn <karlwarner.dev@gmail.com>
2020-07-29 10:59:35 +0100

    converted OrderSummary.js to functional component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from '../../UI/Button/Button'
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(key => (
-      <p key={key}>
-        <span style={{ textTransform: 'capitalize' }}>{key}</span>: £
-        {this.props.ingredients[key]}
-      </p>
-    ))
-    return (
-      <>
-        <h3>Your Order</h3>
-        <p>Burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>Continue to checkout?</p>
-        <h1>Total: £{this.props.totalPrice.toFixed(2)}</h1>
-        <Button btnType="Danger" clicked={this.props.cancelPurchase}>
-          Cancel
-        </Button>
-        <Button
-          btnType="Success"
-          clicked={this.props.confirmPurchase}
-          style={{ justifyContent: 'right' }}
-        >
-          Continue
-        </Button>
-      </>
-    )
-  }
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(key => (
+    <p key={key}>
+      <span style={{ textTransform: 'capitalize' }}>{key}</span>: £
+      {props.ingredients[key]}
+    </p>
+  ))
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>Burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout?</p>
+      <h1>Total: £{props.totalPrice.toFixed(2)}</h1>
+      <Button btnType="Danger" clicked={props.cancelPurchase}>
+        Cancel
+      </Button>
+      <Button
+        btnType="Success"
+        clicked={props.confirmPurchase}
+        style={{ justifyContent: 'right' }}
+      >
+        Continue
+      </Button>
+    </>
+  )
 }
 
 export default OrderSummary
